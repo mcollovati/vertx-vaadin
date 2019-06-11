82efd40528b18dc0144cccba64cbbfcedb09ab42
Marco Collovati <mcollovati@gmail.com>
2019-06-11 22:50:08 +0200

    Dont fail building sockjs clients

--- tools/build_flow_widgetsets.sh
+++ tools/build_flow_widgetsets.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 _base_dir=$(dirname $(realpath $0))
+_action=${1:package}
 
 vaadin_releases=(10)
 for rel in  ${vaadin_releases[@]}; do
@@ -16,7 +17,7 @@ for rel in  ${vaadin_releases[@]}; do
             cat target/flow-client.version | grep 'com.vaadin:flow-client' | cut -d ':' -f 4)
 
         echo "Deploying vaadin-flow-sockjs for vaadin ${version}, flow client ${flow_client_version}"
-        $_base_dir/../mvnw -B -Prelease-flow -pl :vaadin-flow-sockjs -Dvertx-vaadin.release -DskipTests -Dvaadin.platform.version=${version} -Dvaadin.flow.version=${flow_client_version} clean deploy
+        $_base_dir/../mvnw -B --fail-never -Prelease-flow -pl :vaadin-flow-sockjs -Dvertx-vaadin.release -DskipTests -Dvaadin.platform.version=${version} -Dvaadin.flow.version=${flow_client_version} clean $_action
     done
 done
 
