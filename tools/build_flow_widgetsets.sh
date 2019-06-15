a5c213b154adc729cfb2d357a4b3401d77e1a4fa
Marco Collovati <mcollovati@gmail.com>
2019-06-15 15:16:46 +0200

    Reuse prebuilt sockjs client

--- tools/build_flow_widgetsets.sh
+++ tools/build_flow_widgetsets.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-_base_dir=$(dirname $(realpath $0))
+_base_dir="$(dirname $(realpath $0))/.."
 _action=${1:package}
+_mvn="$_base_dir/mvnw -f $_base_dir/pom.xml"
 
 vaadin_releases=(13)
 for rel in  ${vaadin_releases[@]}; do
@@ -9,15 +10,25 @@ for rel in  ${vaadin_releases[@]}; do
     echo "Fetch Vaadin versions for ${rel}..."
     versions=$(curl -s "https://search.maven.org/solrsearch/select?q=g:com.vaadin+AND+a:vaadin-core+AND+v:${rel}.*&rows=10&core=gav" | jq -r '.response.docs[].v')
 
+    _last_built=""
     for version in ${versions}; do
 
         echo "Find flow-client version for vaadin ${version}..."
-        flow_client_version=$(mvn -Pfind-flow-client-version -q dependency:list -Dvaadin.platform.version=${version} \
-            -DincludeArtifactIds=flow-client -DoutputFile=target/flow-client.version && \
-            cat target/flow-client.version | grep 'com.vaadin:flow-client' | cut -d ':' -f 4)
+        flow_client_version=$($_mvn -Pfind-flow-client-version -q dependency:list -Dvaadin.platform.version=${version} \
+            -DincludeArtifactIds=flow-client -DoutputFile=$_base_dir/target/flow-client.version && \
+            cat $_base_dir/target/flow-client.version | grep 'com.vaadin:flow-client' | cut -d ':' -f 4)
 
-        echo "Deploying vaadin-flow-sockjs for vaadin ${version}, flow client ${flow_client_version}"
-        $_base_dir/../mvnw -B --fail-never -Prelease-flow -pl :vaadin-flow-sockjs -Dvertx-vaadin.release -DskipTests -Dvaadin.platform.version=${version} -Dvaadin.flow.version=${flow_client_version} clean $_action
+        _mvn_target="$_action"
+        if [[ "${_last_built}" = "${flow_client_version}" ]]; then
+            echo "Deploy already built vaadin-flow-sockjs based on ${flow_client_version} for vaadin ${version}"
+            _mvn_target="-DskipBuild=true $_action"
+        else
+            echo "Deploying vaadin-flow-sockjs for vaadin ${version}, flow client ${flow_client_version}"
+            _mvn_target="clean $_action"
+        fi
+
+        $_mvn -B --fail-never -Prelease-flow -pl :vaadin-flow-sockjs -Dvertx-vaadin.release -DskipTests -Dvaadin.platform.version=${version} -Dvaadin.flow.version=${flow_client_version} $_mvn_target
+        _last_built=${flow_client_version}
     done
 done
 
