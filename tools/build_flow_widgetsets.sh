f09899e7caa5e223ba658cf345297438acaa587e
Marco Collovati <mcollovati@gmail.com>
2019-06-16 12:33:46 +0200

    Build sockjs client also for pre-releases

--- tools/build_flow_widgetsets.sh
+++ tools/build_flow_widgetsets.sh
@@ -4,11 +4,30 @@ _base_dir="$(dirname $(realpath $0))/.."
 _action=${1:package}
 _mvn="$_base_dir/mvnw -f $_base_dir/pom.xml"
 
-vaadin_releases=(13)
-for rel in  ${vaadin_releases[@]}; do
 
-    echo "Fetch Vaadin versions for ${rel}..."
-    versions=$(curl -s "https://search.maven.org/solrsearch/select?q=g:com.vaadin+AND+a:vaadin-core+AND+v:${rel}.*&rows=10&core=gav" | jq -r '.response.docs[].v')
+function get_vaadin_versions() {
+    local __result=$1
+    local vaadin_platform_full=$($_mvn -N help:evaluate -q -Dexpression='vaadin.platform.version' -DforceStdout=true)
+    local vaadin_platform=$(echo $vaadin_platform_full | cut -d '.' -f 1)
+    local __versions
+    echo "Fetch Vaadin versions for ${vaadin_platform}..."
+    case $vaadin_platform_full in
+        *-SNAPSHOT)
+        __versions=$(curl -s https://maven.vaadin.com/vaadin-prereleases/com/vaadin/vaadin-core/maven-metadata.xml | grep "<version>${vaadin_platform}\..*</version>" | sed -E 's/^.*<version>(.*)<\/version>.*/\1/g')
+        ;;
+        *)
+        __versions=$(curl -s "https://search.maven.org/solrsearch/select?q=g:com.vaadin+AND+a:vaadin-core+AND+v:${vaadin_platform}.*&rows=10&core=gav" | jq -r '.response.docs[].v')
+        ;;
+    esac
+    echo "Found versions: ${__versions}"
+    eval $__result="'${__versions}'"
+}
+vaadin_releases=$($_mvn -N help:evaluate -q -Dexpression='vaadin.platform.version' -DforceStdout=true | cut -d '.' -f 1)
+#for rel in  ${vaadin_releases[@]}; do
+
+#    echo "Fetch Vaadin versions for ${rel}..."
+#    versions=$(curl -s "https://search.maven.org/solrsearch/select?q=g:com.vaadin+AND+a:vaadin-core+AND+v:${rel}.*&rows=10&core=gav" | jq -r '.response.docs[].v')
+    get_vaadin_versions "versions"
 
     _last_built=""
     for version in ${versions}; do
@@ -27,8 +46,9 @@ for rel in  ${vaadin_releases[@]}; do
             _mvn_target="clean $_action"
         fi
 
-        $_mvn -B --fail-never -Prelease-flow -pl :vaadin-flow-sockjs -Dvertx-vaadin.release -DskipTests -Dvaadin.platform.version=${version} -Dvaadin.flow.version=${flow_client_version} $_mvn_target
+        echo $_mvn -B --fail-never -Prelease-flow -pl :vaadin-flow-sockjs -Dvertx-vaadin.release -DskipTests -Dvaadin.platform.version=${version} -Dvaadin.flow.version=${flow_client_version} $_mvn_target
         _last_built=${flow_client_version}
     done
-done
+#done
+
 
