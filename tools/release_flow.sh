58692285e9d90e74298eabccab3c60e0b57b6de8
Marco Collovati <mcollovati@gmail.com>
2019-09-12 21:39:58 +0200

    Release vertx-vaadin 14.0.0

---
+++ tools/release_flow.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+_base_dir="$(dirname $(realpath $0))/.."
+_newVersion=${1:?New version is missing}
+_mvn="$_base_dir/mvnw -f $_base_dir/pom.xml"
+
+$_mvn -Prelease-flow -pl :vaadin-flow-sockjs versions:set -DnewVersion=${_newVersion}
+$_mvn -Prelease-flow -pl :vertx-vaadin-flow versions:set -DnewVersion=${_newVersion}
