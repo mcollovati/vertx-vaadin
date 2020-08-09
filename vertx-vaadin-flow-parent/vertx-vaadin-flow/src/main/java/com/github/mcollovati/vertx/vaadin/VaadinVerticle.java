956c8893d3f3a2802936cc208f45fd34a693e8eb
Marco Collovati <mcollovati@gmail.com>
2020-08-09 16:17:26 +0200

    Stop DevModeHandler on shutdown

--- vertx-vaadin-flow-parent/vertx-vaadin-flow/src/main/java/com/github/mcollovati/vertx/vaadin/VaadinVerticle.java
+++ vertx-vaadin-flow-parent/vertx-vaadin-flow/src/main/java/com/github/mcollovati/vertx/vaadin/VaadinVerticle.java
@@ -199,6 +199,11 @@ public class VaadinVerticle extends AbstractVerticle {
             log.error("Error during Vaadin service destroy", ex);
         }
 
+        DevModeHandler handler = DevModeHandler.getDevModeHandler();
+        if (handler != null && !handler.reuseDevServer()) {
+            handler.stop();
+        }
+
         try {
             // Perform potential synchronous clean-up tasks
             stop();
