93cb3a5cc03d0d5509f89da0541da05bad13d20c
Marco Collovati <mcollovati@gmail.com>
2022-03-28 21:48:55 +0200

    Added missin test files

---
+++ vertx-vaadin-flow-parent/vertx-vaadin-flow/src/test/java/com/github/mcollovati/failures/lookup/FakeLookupInitializer2.java
@@ -0,0 +1,17 @@
+package com.github.mcollovati.failures.lookup;
+
+import java.util.Collection;
+import java.util.Map;
+
+import com.vaadin.flow.di.AbstractLookupInitializer;
+import com.vaadin.flow.function.VaadinApplicationInitializationBootstrap;
+import com.vaadin.flow.server.VaadinContext;
+
+// Added to classpath just to test initialization failure
+public class FakeLookupInitializer2 implements AbstractLookupInitializer {
+
+    @Override
+    public void initialize(VaadinContext context, Map<Class<?>, Collection<Class<?>>> services, VaadinApplicationInitializationBootstrap bootstrap) {
+        // do nothing
+    }
+}
