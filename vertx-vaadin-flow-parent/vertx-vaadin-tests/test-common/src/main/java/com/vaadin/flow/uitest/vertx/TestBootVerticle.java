2b63eebaac2bd985ff35273d3b1447a42599f930
Marco Collovati <mcollovati@gmail.com>
2022-05-06 21:25:02 +0200

    Patch TaskRunNpmInstall
    to avoid EPIPE -32 error on npm install

--- vertx-vaadin-flow-parent/vertx-vaadin-tests/test-common/src/main/java/com/vaadin/flow/uitest/vertx/TestBootVerticle.java
+++ vertx-vaadin-flow-parent/vertx-vaadin-tests/test-common/src/main/java/com/vaadin/flow/uitest/vertx/TestBootVerticle.java
@@ -1,23 +1,70 @@
 package com.vaadin.flow.uitest.vertx;
 
+import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Field;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 import com.github.mcollovati.vertx.vaadin.VaadinVerticle;
 import com.github.mcollovati.vertx.vaadin.VertxVaadinService;
-import com.vaadin.flow.server.DevModeHandler;
-import com.vaadin.flow.server.VaadinService;
 import io.vertx.core.http.HttpServerResponse;
 import io.vertx.core.json.JsonArray;
 import io.vertx.ext.web.Router;
-import nonapi.io.github.classgraph.utils.ReflectionUtils;
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.agent.ByteBuddyAgent;
+import net.bytebuddy.dynamic.loading.ClassReloadingStrategy;
+import net.bytebuddy.implementation.MethodDelegation;
+import net.bytebuddy.matcher.ElementMatchers;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.vaadin.flow.server.DevModeHandler;
+import com.vaadin.flow.server.VaadinService;
+import com.vaadin.flow.server.frontend.FrontendUtils;
+import com.vaadin.flow.server.frontend.TaskRunNpmInstall;
 
 public class TestBootVerticle extends VaadinVerticle {
 
     private static final Map<String, ViewClassLocator> viewLocators = new HashMap<>();
+    public static final Logger LOGGER = LoggerFactory.getLogger(TestBootVerticle.class);
 
+    // Patching TaskRunNpmInstall to redirect STDERR through PIPE
+    // to avoid EPIPE error -32 during npm install
+    static {
+        ByteBuddyAgent.install();
+        new ByteBuddy().redefine(TaskRunNpmInstall.class)
+            .method(ElementMatchers.named("runNpmCommand"))
+            .intercept(MethodDelegation.to(Target.class))
+            .make()
+            .load(
+                TaskRunNpmInstall.class.getClassLoader(),
+                ClassReloadingStrategy.fromInstalledAgent());
+    }
+
+    public static final class Target {
+        public static Process runNpmCommand(List<String> command, File workingDirectory)
+            throws IOException {
+            ProcessBuilder builder = FrontendUtils.createProcessBuilder(command);
+            builder.environment().put("ADBLOCK", "1");
+            builder.environment().put("NO_UPDATE_NOTIFIER", "1");
+            builder.directory(workingDirectory);
+            builder.redirectInput(ProcessBuilder.Redirect.PIPE);
+            builder.redirectError(ProcessBuilder.Redirect.PIPE);
+
+            Process process = builder.start();
+
+            // This will allow to destroy the process which does IO regardless
+            // whether it's executed in the same thread or another (may be
+            // daemon) thread
+            Runtime.getRuntime()
+                .addShutdownHook(new Thread(process::destroyForcibly));
+
+            return process;
+        }
+    }
     @Override
     protected void serviceInitialized(VertxVaadinService service, Router router) {
         String mountPoint = service.getVaadinOptions().mountPoint();
@@ -30,27 +77,29 @@ public class TestBootVerticle extends VaadinVerticle {
                     .replaceFirst(alias + "/", mountPoint + "/"));
             }));
         TestBootVerticle.viewLocators.put(deploymentID(), new ViewClassLocator(getClass().getClassLoader()));
-        router.get("/__check-start").handler(ctx -> {
-            System.out.println("======================== check start");
+        router.get("/__check-start").order(0).handler(ctx -> {
+            LOGGER.trace("======================== check start");
             HttpServerResponse response = ctx.response();
             DevModeHandler devModeHandler = DevModeHandler.getDevModeHandler();
             response.setStatusCode(404);
             if (devModeHandler != null) {
-                System.out.println("======================== check start. dev mod 1");
+                LOGGER.trace("======================== check start. dev mod 1");
                 try {
                     Field startFutureField = devModeHandler.getClass().getDeclaredField("devServerStartFuture");
                     startFutureField.setAccessible(true);
                     CompletableFuture<?> o = (CompletableFuture)startFutureField.get(devModeHandler);
                     if (o.isDone()) {
-                        System.out.println("======================== check start. dev mod 2");
+                        LOGGER.info("DevModeHandler ready");
                         response.setStatusCode(200);
                     }
                 } catch (NoSuchFieldException | IllegalAccessException e) {
                     e.printStackTrace();
-                    System.out.println("======================== check start. dev mod err ");
+                    LOGGER.trace("======================== check start. dev mod err ");
                 }
+            } else if (service.getDeploymentConfiguration().isProductionMode()) {
+                response.setStatusCode(200);
             }
-            System.out.println("======================== check start -> " + response.getStatusCode());
+            LOGGER.trace("======================== check start -> " + response.getStatusCode());
             response.end();
         });
     }
