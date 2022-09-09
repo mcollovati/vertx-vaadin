c098476b383775eb7777a57e581951ed7ce50dac
Marco Collovati <mcollovati@gmail.com>
2022-09-09 10:56:23 +0200

    fix: fix debug window websocket handling
    Fixes #69

--- vertx-vaadin-flow-parent/vertx-vaadin-flow/src/main/java/com/github/mcollovati/vertx/vaadin/communication/VertxDebugWindowConnection.java
+++ vertx-vaadin-flow-parent/vertx-vaadin-flow/src/main/java/com/github/mcollovati/vertx/vaadin/communication/VertxDebugWindowConnection.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 import com.github.mcollovati.vertx.vaadin.VertxVaadinService;
@@ -14,11 +15,9 @@ import com.vaadin.base.devserver.FeatureFlagMessage;
 import com.vaadin.base.devserver.ServerInfo;
 import com.vaadin.base.devserver.stats.DevModeUsageStatistics;
 import com.vaadin.experimental.FeatureFlags;
-import com.vaadin.flow.shared.ApplicationConstants;
 import com.vaadin.pro.licensechecker.LicenseChecker;
 import com.vaadin.pro.licensechecker.Product;
 import elemental.json.JsonObject;
-import io.vertx.core.eventbus.MessageProducer;
 import io.vertx.core.http.ServerWebSocket;
 import io.vertx.core.json.Json;
 import org.slf4j.Logger;
@@ -26,11 +25,11 @@ import org.slf4j.LoggerFactory;
 
 public class VertxDebugWindowConnection implements VertxVaadinLiveReload {
 
-    private static final Logger logger = LoggerFactory.getLogger(VertxDebugWindowConnection.class);
+    private static final Logger logger = LoggerFactory
+            .getLogger(VertxDebugWindowConnection.class);
 
     private VertxVaadinService service;
-    private final Map<String, MessageProducer<String>> liveReload = new ConcurrentHashMap<>();
-
+    private final Map<String, Consumer<String>> liveReload = new ConcurrentHashMap<>();
 
     public VertxDebugWindowConnection() {
     }
@@ -43,24 +42,17 @@ public class VertxDebugWindowConnection implements VertxVaadinLiveReload {
         this.service = service;
     }
 
-
-    public void handle(ServerWebSocket webSocket) {
-        if (isDebugWindowConnection(webSocket)) {
-            String websocketId = webSocket.textHandlerID();
-            liveReload.put(websocketId, service.getVertx().eventBus().publisher(websocketId));
-            webSocket.textMessageHandler(message -> onMessage(websocketId, message));
-            webSocket.closeHandler(unused -> onClose(websocketId));
-
-            webSocket.writeTextMessage("{\"command\": \"hello\"}");
-            sendDebugWindowCommand(webSocket, "serverInfo", new ServerInfo());
-            sendDebugWindowCommand(webSocket, "featureFlags", new FeatureFlagMessage(FeatureFlags
+    public void onConnect(String websocketId, Consumer<String> producer) {
+        this.liveReload.put(websocketId, producer);
+        producer.accept("{\"command\": \"hello\"}");
+        send(websocketId, "serverInfo", new ServerInfo());
+        send(websocketId, "featureFlags", new FeatureFlagMessage(FeatureFlags
                 .get(service.getContext()).getFeatures().stream()
                 .filter(feature -> !feature.equals(FeatureFlags.EXAMPLE))
                 .collect(Collectors.toList())));
-        }
     }
 
-    private void onMessage(String websocketId, String message) {
+    public void onMessage(String websocketId, String message) {
         if (message.isEmpty()) {
             logger.debug("Received live reload heartbeat");
             return;
@@ -69,8 +61,8 @@ public class VertxDebugWindowConnection implements VertxVaadinLiveReload {
         String command = json.getString("command");
         if ("setFeature".equals(command)) {
             elemental.json.JsonObject data = json.getObject("data");
-            FeatureFlags.get(service.getContext()).setEnabled(data.getString("featureId"),
-                data.getBoolean("enabled"));
+            FeatureFlags.get(service.getContext()).setEnabled(
+                    data.getString("featureId"), data.getBoolean("enabled"));
         } else if ("reportTelemetry".equals(command)) {
             elemental.json.JsonObject data = json.getObject("data");
             DevModeUsageStatistics.handleBrowserData(data);
@@ -84,10 +76,10 @@ public class VertxDebugWindowConnection implements VertxVaadinLiveReload {
 
             try {
                 LicenseChecker.checkLicense(product.getName(),
-                    product.getVersion(), keyUrl -> {
-                        send(websocketId, "license-check-nokey",
-                            new ProductAndMessage(product, keyUrl));
-                    });
+                        product.getVersion(), keyUrl -> {
+                            send(websocketId, "license-check-nokey",
+                                    new ProductAndMessage(product, keyUrl));
+                        });
                 ok = true;
             } catch (Exception e) {
                 ok = false;
@@ -97,7 +89,7 @@ public class VertxDebugWindowConnection implements VertxVaadinLiveReload {
                 send(websocketId, "license-check-ok", product);
             } else {
                 ProductAndMessage pm = new ProductAndMessage(product,
-                    errorMessage);
+                        errorMessage);
                 send(websocketId, "license-check-failed", pm);
             }
         } else {
@@ -105,50 +97,40 @@ public class VertxDebugWindowConnection implements VertxVaadinLiveReload {
         }
     }
 
-    private void send(String websocketId, String command,
-                      Object data) {
+    private void send(String websocketId, String command, Object data) {
         try {
             Optional.ofNullable(liveReload.get(websocketId))
-                .ifPresent(producer -> producer.write(Json.encode(new DebugWindowMessage(command, data))));
+                    .ifPresent(producer -> producer.accept(Json
+                            .encode(new DebugWindowMessage(command, data))));
         } catch (Exception e) {
             getLogger().error("Error sending message", e);
         }
     }
 
-
-    private void onClose(String websocketId) {
+    public void onClose(String websocketId) {
         logger.debug("Live reload connection disconnected for {}", websocketId);
-        Optional.ofNullable(liveReload.get(websocketId)).ifPresent(MessageProducer::close);
+        // Optional.ofNullable(liveReload.get(websocketId)).ifPresent(MessageProducer::close);
+        liveReload.remove(websocketId);
     }
 
     public void reload() {
-        liveReload.values().stream()
-            .filter(Objects::nonNull)
-            .forEach(socket -> {
-                try {
-                    socket.write("{\"command\": \"reload\"}");
-                } catch (Exception ex) {
-                    // ignore
-                }
-            });
+        liveReload.values().stream().filter(Objects::nonNull)
+                .forEach(socket -> socket.accept("{\"command\": \"reload\"}"));
     }
 
-
-    private void sendDebugWindowCommand(ServerWebSocket webSocket, String command, Object data) {
+    private void sendDebugWindowCommand(ServerWebSocket webSocket,
+            String command, Object data) {
         try {
-            webSocket.writeTextMessage(Json.encode(new DebugWindowMessage(command, data)));
+            webSocket.writeTextMessage(
+                    Json.encode(new DebugWindowMessage(command, data)));
         } catch (Exception e) {
             logger.error("Error sending message", e);
         }
     }
 
-    private boolean isDebugWindowConnection(ServerWebSocket webSocket) {
-        return service.getDeploymentConfiguration().isDevModeLiveReloadEnabled()
-            && webSocket.query().contains(ApplicationConstants.DEBUG_WINDOW_CONNECTION);
-    }
-
     private static Logger getLogger() {
-        return LoggerFactory.getLogger(VertxDebugWindowConnection.class.getName());
+        return LoggerFactory
+                .getLogger(VertxDebugWindowConnection.class.getName());
     }
 
     static class ProductAndMessage implements Serializable {
