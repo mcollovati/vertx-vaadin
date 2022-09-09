a7c0ccb2e6a7f1fbab21efe6b1893dcc748d0510
Marco Collovati <mcollovati@gmail.com>
2022-09-09 11:22:04 +0200

    fix: patched ViteHandler to get rid of VaadinServletContext explicit cast
    Fixes #70

---
+++ vertx-vaadin-flow-parent/vertx-vaadin-flow/src/test/java/com/github/mcollovati/vertx/support/VaadinPatchesTest.java
@@ -0,0 +1,11 @@
+package com.github.mcollovati.vertx.support;
+
+import org.junit.Test;
+
+public class VaadinPatchesTest {
+
+    @Test
+    public void testPatchInstallation() {
+        VaadinPatches.patch();
+    }
+}
