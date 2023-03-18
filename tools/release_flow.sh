dc02edf73e74f408b62224d9e07236e07c8a7a7c
Marco Collovati <mcollovati@gmail.com>
2023-03-18 16:34:04 +0100

    fix version update tool

--- tools/release_flow.sh
+++ tools/release_flow.sh
@@ -6,4 +6,6 @@ _mvn="$_base_dir/mvnw -f $_base_dir/pom.xml"
 
 $_mvn -Prelease-flow -pl :vaadin-flow-sockjs versions:set -DnewVersion=${_newVersion}
 $_mvn -Prelease-flow -pl :vertx-vaadin-flow versions:set -DnewVersion=${_newVersion}
+$_mvn -Prelease-flow -pl :vertx-vaadin-flow-jandex versions:set -DnewVersion=${_newVersion}
+$_mvn -Prelease-flow -pl :vertx-vaadin-quarkus-extension-parent versions:set -DnewVersion=${_newVersion}
 $_mvn -Prelease-flow,flow-ui-tests -pl :vertx-vaadin-test versions:set -DnewVersion=${_newVersion}
