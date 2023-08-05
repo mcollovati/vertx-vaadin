3e2bbe8183fde648c47463d52a1a90586dda28f4
Marco Collovati <mcollovati@gmail.com>
2023-08-05 14:08:34 +0200

    chore: publish classifiers on github pacakges

--- tools/build_flow_widgetsets.sh
+++ tools/build_flow_widgetsets.sh
@@ -1,49 +1,80 @@
 #!/usr/bin/env bash
 
+#######
+# Usage $0 <action> <kind>
+#
+#
+# action : maven goal to execute, defaults to 'package'
+# kind   : artifact type (snapshot, release), defaults to 'release'
+#
+# cat maven-metadata.xml | grep "<version>" | sed 's/^.*<version>\([^<]*\)<.*/\1/g' | sort -r
+# curl -s https://repo1.maven.org/maven2/com/vaadin/vaadin-core/maven-metadata.xml | grep "<version>23\..*</version>" | sed -E 's/^.*<version>(.*)<\/version>.*/\1/g' | sort -r -t '.'
+#######
+
 _base_dir="$(dirname $(realpath $0))/.."
 _action=${1:-package}
 _kind=${2:-release}
 _mvn="$_base_dir/mvnw -f $_base_dir/pom.xml"
 
+if [[ -z $GITHUB_TOKEN ]]; then
+  echo "GITHUB_TOKEN environment variable not provided"
+  exit 1
+fi
+
 _current_version=$($_mvn -Prelease-flow -pl :vaadin-flow-sockjs help:evaluate -q -Dexpression='project.version' -DforceStdout=true)
 
+declare -ga versions
 function get_vaadin_versions() {
-    local __result=$1
+    #local __result=$1
     local vaadin_platform_full=$($_mvn -N help:evaluate -q -Dexpression='vaadin.platform.version' -DforceStdout=true)
-    local vaadin_platform=$(echo $vaadin_platform_full | cut -d '.' -f 1)
+    local vaadin_platform=$(echo $vaadin_platform_full | cut -d '.' -f 1,2)
     local __versions
     echo "Fetch Vaadin versions for ${vaadin_platform}..."
+    local __metadata_base_url
     case $vaadin_platform_full in
         *-SNAPSHOT|*.alpha*|*.beta*|*.rc*)
-        __versions=$(curl -s https://maven.vaadin.com/vaadin-prereleases/com/vaadin/vaadin-core/maven-metadata.xml | grep "<version>${vaadin_platform}\..*</version>" | sed -E 's/^.*<version>(.*)<\/version>.*/\1/g')
+        __metadata_base_url=https://maven.vaadin.com/vaadin-prereleases
         ;;
         *)
-        __versions=$(curl -s "https://search.maven.org/solrsearch/select?q=g:com.vaadin+AND+a:vaadin-core+AND+v:${vaadin_platform}.*&rows=10&core=gav" | jq -r '.response.docs | sort_by(.v|split(".")|map(tonumber)) | reverse | .[].v')
+        __metadata_base_url=https://repo1.maven.org/maven2
         ;;
     esac
+    __versions=$(curl -s ${__metadata_base_url}/com/vaadin/vaadin-core/maven-metadata.xml | \
+      grep "<version>${vaadin_platform}\..*</version>" | sed -E 's/^.*<version>(.*)<\/version>.*/\1/g' | \
+      sort -V -r | head -n 100
+    )
     echo "Found Vaadin versions:"
     echo "${__versions}"
     echo
-    eval $__result="'${__versions}'"
+    for v in ${__versions}; do
+      versions+=("$v")
+    done
+    #eval $__result="(${__versions}'"
 }
-get_vaadin_versions "versions"
+get_vaadin_versions #"versions"
 
-## Fetch existing classifiers
-URL="https://mcollovati.jfrog.io/artifactory/api/search/gavc?g=com.github.mcollovati.vertx&a=vaadin-flow-sockjs&c=vaadin-*&v=${_current_version}&repos=vertx-vaadin-releases"
-JQ_FILTER='.results[].uri | (match("^.*/vaadin-flow-sockjs-'${_current_version}'-vaadin-(?<version>.*)\\.jar$") | .captures[].string )'
-
-__classifiers=$(curl -s "${URL}" | jq --raw-output "${JQ_FILTER}" | sort -r)
-echo "Existing classifiers for ${_current_version}:"
-echo "${__classifiers[@]}"
-echo
+###versions=("${versions[@]:9:3}")
+echo "Search for existing classifiers..."
 declare -A _existing_versions
-for classifier in ${__classifiers}; do
-  _existing_versions[${classifier}]=1
+for version in "${versions[@]}"; do
+
+  flag=$(
+    mvn -q dependency:get -Dartifact=com.github.mcollovati.vertx:vaadin-flow-sockjs:${_current_version}:jar:vaadin-${version} \
+      -DremoteRepositories=github-vertx-vaadin::::https://maven.pkg.github.com/mcollovati/vertx-vaadin/ -Dtransitive=false 2>&1 >/dev/null && \
+      echo 1 || echo 0
+  )
+  echo "  --> Searching classifier vaadin-${version} for version ${_current_version} ===> ${flag}"
+  if [[ "${flag}" = "1" ]]; then
+    _existing_versions[$version]=$version
+  fi
 done
 
+echo "Existing classifiers for ${_current_version}:"
+echo "${_existing_versions[@]}"
+echo
 
 _last_built=""
-for version in ${versions}; do
+for version in "${versions[@]}"; do
 
   if [[ ${_existing_versions[$version]} ]]; then
     echo "Classifier vaadin-${version} already exists for version ${_current_version}"
