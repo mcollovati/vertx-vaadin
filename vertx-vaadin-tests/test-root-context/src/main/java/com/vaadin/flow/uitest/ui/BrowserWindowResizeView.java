6140f3ec44ba1ebda97b5cb397ec13f8c05cb45e
Marco Collovati <mcollovati@gmail.com>
2024-07-20 09:29:19 +0200

    fix compilation errors

--- vertx-vaadin-tests/test-root-context/src/main/java/com/vaadin/flow/uitest/ui/BrowserWindowResizeView.java
+++ vertx-vaadin-tests/test-root-context/src/main/java/com/vaadin/flow/uitest/ui/BrowserWindowResizeView.java
@@ -22,6 +22,7 @@
  */
 package com.vaadin.flow.uitest.ui;
 
+import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.NativeButton;
 import com.vaadin.flow.router.Route;
@@ -37,14 +38,14 @@ public class BrowserWindowResizeView extends AbstractDivView {
         windowSize.setId("size-info");
 
         getPage().addBrowserWindowResizeListener(event -> windowSize.setText(
-                "%sx%s".formatted(event.getWidth(), event.getHeight())));
+                String.format("%sx%s", event.getWidth(), event.getHeight())));
 
         add(windowSize);
 
         var modalBtn = new NativeButton("Open modal (should keep working");
         modalBtn.setId("modal");
         modalBtn.addClickListener(e -> {
-            add(new Div("Now modal, but resize events should still flow in"));
+            add(new Div(new Text("Now modal, but resize events should still flow in")));
             getUI().get().addModal(new Div());
         });
         add(modalBtn);
