c714eeb798a039f059973b33e8411780873008c6
Marco Collovati <mcollovati@gmail.com>
2024-08-22 17:49:12 +0200

    bump project version

--- tools/release_flow.sh
+++ tools/release_flow.sh
@@ -4,8 +4,4 @@ _base_dir="$(dirname $(realpath $0))/.."
 _newVersion=${1:?New version is missing}
 _mvn="$_base_dir/mvnw -f $_base_dir/pom.xml"
 
-$_mvn -Prelease-flow -pl :vaadin-flow-sockjs versions:set -DnewVersion=${_newVersion}
-$_mvn -Prelease-flow -pl :vertx-vaadin-flow versions:set -DnewVersion=${_newVersion}
-$_mvn -Prelease-flow -pl :vertx-vaadin-flow-jandex versions:set -DnewVersion=${_newVersion}
-$_mvn -Prelease-flow -pl :vertx-vaadin-quarkus-extension-parent versions:set -DnewVersion=${_newVersion}
-$_mvn -Prelease-flow,flow-ui-tests -pl :vertx-vaadin-test versions:set -DnewVersion=${_newVersion}
+$_mvn -Pflow-ui-tests versions:set -DnewVersion=${_newVersion}
